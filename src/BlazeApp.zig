330a7914ca3a421aafb25c4d48f4be08436d5b41
Adam Brykajlo <adam.brykajlo@gmail.com>
2025-07-28 21:20:39 -0700

    Refactor device creation logic

--- src/BlazeApp.zig
+++ src/BlazeApp.zig
@@ -7,12 +7,14 @@ pub const c = @cImport({
 });
 
 const vk = @import("vulkan/vk.zig");
+const Device = @import("device.zig").Device;
+const DeviceRequirements = @import("device.zig").Requirements;
 
 const BlazeApp = @This();
 
 instance: vk.Instance,
 app_info: vk.ApplicationInfo,
-device: vk.Device,
+device: Device,
 allocator: Allocator,
 
 pub fn init(allocator: Allocator, app_name: []const u8, app_version: vk.Version) !BlazeApp {
@@ -25,7 +27,7 @@ pub fn init(allocator: Allocator, app_name: []const u8, app_version: vk.Version)
 
 pub fn deinit(self: *BlazeApp) void {
     defer self.instance.destroy();
-    defer self.device.destroy();
+    defer self.device.deinit();
 }
 
 fn createInstance(self: *BlazeApp, allocator: Allocator, app_name: []const u8, app_version: vk.Version) !void {
@@ -61,69 +63,15 @@ fn createDevice(self: *BlazeApp) !void {
     const physical_devices = try self.instance.enumeratePhysicalDevices(self.allocator);
     defer self.allocator.free(physical_devices);
 
-    var queue_create_infos: std.ArrayList(vk.Device.QueueCreateInfo) = .init(self.allocator);
-    defer queue_create_infos.deinit();
-
-    var found_suitable_device = false;
-    var suitable_device: vk.PhysicalDevice = undefined;
-
+    const device_req: DeviceRequirements = .{ .graphicsSupport = true, .presentationSupport = true, .integratedGPU = true };
     for (physical_devices) |physical_device| {
-        const queue_family_properties = try physical_device.getQueueFamilyProperties(self.allocator);
-        defer self.allocator.free(queue_family_properties);
-
-        // check for required queue family support
-        var graphics_idx: ?u32 = null;
-        var presentation_idx: ?u32 = null;
-        for (queue_family_properties, 0..) |*properties, i| {
-            const idx: u32 = @intCast(i);
-            const graphics_support = properties.queueFlags.queue_graphics_bit;
-            graphics_idx = if (graphics_idx == null and graphics_support) idx else null;
-
-            const presentation_support = c.SDL_Vulkan_GetPresentationSupport(@ptrCast(self.instance.ptr), @ptrCast(physical_device.ptr), idx);
-            presentation_idx = if (presentation_idx == null and presentation_support) idx else null;
-
-            if (graphics_idx != null and presentation_idx != null) {
-                break;
-            }
+        if (try device_req.queryPhysicalDevice(self.allocator, self.instance, physical_device)) |queues| {
+            self.device = try .init(self.allocator, physical_device, &device_req, &queues);
+            return;
         }
-
-        // if we didn't find the necessary queues we move to the next physical_device
-        if (graphics_idx == null or presentation_idx == null) {
-            continue;
-        }
-
-        found_suitable_device = true;
-        suitable_device = physical_device;
-
-        // set up unique queues
-        var queue_set: std.DynamicBitSet = try .initEmpty(self.allocator, queue_family_properties.len);
-        defer queue_set.deinit();
-
-        // setup graphics queue
-        queue_set.set(graphics_idx.?);
-
-        // setup presentation queue
-        queue_set.set(presentation_idx.?);
-
-        var iter = queue_set.iterator(.{ .direction = .forward, .kind = .set });
-        while (iter.next()) |queue_family_index| {
-            const create_info = try queue_create_infos.addOne();
-            const queue_priorities: [1]f32 = .{1.0};
-            create_info.* = .{ .queueFamilyIndex = @intCast(queue_family_index), .queueCount = 1, .pQueuePriorities = &queue_priorities };
-        }
-
-        break;
     }
 
-    if (!found_suitable_device) {
-        return error.NoSuitableDevice;
-    }
-
-    var device_create_info: vk.Device.CreateInfo = .{};
-    device_create_info.queueCreateInfoCount = @intCast(queue_create_infos.items.len);
-    device_create_info.pQueueCreateInfos = @ptrCast(queue_create_infos.items);
-
-    self.device = try suitable_device.createDevice(&device_create_info);
+    return error.NoSuitableDevice;
 }
 
 fn checkValidationLayers(self: *const BlazeApp) !void {
---
+++ src/device.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+pub const c = @cImport({
+    @cInclude("SDL3/SDL_vulkan.h");
+});
+
+const vk = @import("vulkan/vk.zig");
+
+pub const Device = struct {
+    device: vk.Device,
+    queues: Queues,
+
+    pub fn init(allocator: Allocator, physical_device: vk.PhysicalDevice, requirements: *const Requirements, queues: *const Queues) !Device {
+        var device: Device = undefined;
+        device.queues = queues.*;
+
+        var queue_create_infos: std.ArrayList(vk.Device.QueueCreateInfo) = .init(allocator);
+        defer queue_create_infos.deinit();
+
+        // set up unique queues
+        var queue_set: std.DynamicBitSet = try .initEmpty(allocator, device.queues.familyCount);
+        defer queue_set.deinit();
+
+        // setup graphics queue
+        if (requirements.graphicsSupport) {
+            queue_set.set(device.queues.graphics.?);
+        }
+
+        // setup presentation queue
+        if (requirements.presentationSupport) {
+            queue_set.set(device.queues.presentation.?);
+        }
+
+        var iter = queue_set.iterator(.{ .direction = .forward, .kind = .set });
+        while (iter.next()) |queue_family_index| {
+            const create_info = try queue_create_infos.addOne();
+            const queue_priorities: [1]f32 = .{1.0};
+            create_info.* = .{ .queueFamilyIndex = @intCast(queue_family_index), .queueCount = 1, .pQueuePriorities = &queue_priorities };
+        }
+
+        var device_create_info: vk.Device.CreateInfo = .{};
+        device_create_info.queueCreateInfoCount = @intCast(queue_create_infos.items.len);
+        device_create_info.pQueueCreateInfos = @ptrCast(queue_create_infos.items);
+
+        device.device = try physical_device.createDevice(&device_create_info);
+        return device;
+    }
+
+    pub fn deinit(self: *Device) void {
+        self.device.destroy();
+    }
+
+    pub fn getGraphicsQueue(self: *const Device) ?vk.Queue {
+        self.device;
+        return null;
+    }
+
+    pub fn getPresentationQueue(self: *const Device) ?vk.Queue {
+        self.device;
+        return null;
+    }
+};
+
+pub const Requirements = struct {
+    graphicsSupport: bool,
+    presentationSupport: bool,
+    integratedGPU: bool,
+
+    pub fn queryPhysicalDevice(self: *const Requirements, allocator: Allocator, instance: vk.Instance, physical_device: vk.PhysicalDevice) !?Queues {
+        if (self.integratedGPU and physical_device.getProperties().deviceType != .integrated_gpu) {
+            return null;
+        }
+
+        const queue_family_properties = try physical_device.getQueueFamilyProperties(allocator);
+        defer allocator.free(queue_family_properties);
+
+        var queues: Queues = .{ .familyCount = @intCast(queue_family_properties.len) };
+
+        if (self.graphicsSupport) {
+            for (queue_family_properties, 0..) |*properties, i| {
+                if (properties.queueFlags.queue_graphics_bit) {
+                    queues.graphics = @intCast(i);
+                    break;
+                }
+            }
+
+            // if graphics queue is not found
+            if (queues.graphics == null) {
+                return null;
+            }
+        }
+
+        if (self.presentationSupport) {
+            for (queue_family_properties, 0..) |_, i| {
+                if (c.SDL_Vulkan_GetPresentationSupport(@ptrCast(instance.ptr), @ptrCast(physical_device.ptr), @intCast(i))) {
+                    queues.presentation = @intCast(i);
+                    break;
+                }
+            }
+
+            // if presentation queue is not found
+            if (queues.presentation == null) {
+                return null;
+            }
+        }
+
+        return queues;
+    }
+};
+
+const Queues = struct {
+    graphics: ?u32 = null,
+    presentation: ?u32 = null,
+    familyCount: u32,
+};
